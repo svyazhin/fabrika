f7a46f24dd61ee13df1bf4d4678b37b63a79fbad
Gleb Svyazhin <svyazhin@MacBook-Pro-Gleb.local>
2016-01-16 14:43:15 +0200

    lesson 2

---
+++ fahrenheit.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace fahrenheit
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			const double K= 1.8;
+			const double j= 32;
+
+			Console.WriteLine ("Type temperature in celsius:");
+			double tC = double.Parse (Console.ReadLine ());
+			double tF = K * tC + j;
+
+			Console.WriteLine ( "Temperature in fahranheit:{0}", tF%d);
+		}
+	}
+}
